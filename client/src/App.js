57b88277d0504f43250dc6669f62fdc9d6f1c81e
Evan Tucker <eej.tucker@gmail.com>
2018-08-24 18:59:04 -0600

    added react app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import GiphBtns from "./components/GiphBtns";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        < GiphBtns />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/GiphBtns/GiphBtns.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class GiphBtns extends React.Component {
+    render() {
+      return <h1>Hello</h1>;
+    }
+}
+
+export default GiphBtns;
